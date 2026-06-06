5e26184fb08b5a4ee80d91819e364192c430d081
Sarth Mishra <mishrasarth2186@gmail.com>
2026-06-06 17:48:54 +0530

    add treenode.java

--- treenode.java
+++ treenode.java
@@ -22,3 +22,34 @@ public class treenode {
         //System.out.print();
     }
 }
+/*class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+class Main {
+    public static void inordertraversal(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        inordertraversal(root.left);
+        System.out.print(root.val + "->");
+        inordertraversal(root.right);
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(7);
+
+        inordertraversal(root);  // Output: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 
+    }
+} */
