84a269eac13ad3d5532956e4d8c47402de94277b
Sarth Mishra <mishrasarth2186@gmail.com>
2026-06-28 23:11:50 +0530

    Remove student query and add delete operation for department records in Database class

--- Database.java
+++ Database.java
@@ -5,22 +5,9 @@ public static void main(String[] args)throws Exception
 {
 Class.forName("org.sqlite.JDBC");
 Connection con  = DriverManager.getConnection("jdbc:sqlite:univ.db");
-PreparedStatement stm = con.prepareStatement("SELECT * FROM student WHERE deptno = ?");
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter Dept Number ");
-int deptno  = sc.nextInt();
-
-stm.setInt(1, deptno);
-ResultSet rs = stm.executeQuery();
-//int dno;
-//String dname;
-while(rs.next())
-{
-System.out.print(rs.getInt("roll")+ " ");
-System.out.print(rs.getString("name")+ " ");
-System.out.print(rs.getString("city")+ " ");
-System.out.print(rs.getInt(4)+ " ");
-}
+Statement stm = con.createStatement();
+stm.executeUpdate("delete from dept where deptno >= 60 ");
 stm.close();
+con.close();
 }
 }
