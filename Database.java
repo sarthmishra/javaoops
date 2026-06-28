ce3f79d304c8a8635cdb5b6322ab2dd8d49aacfa
Sarth Mishra <mishrasarth2186@gmail.com>
2026-06-28 23:29:09 +0530

    Replace delete operation with student data insertion in Database class

--- Database.java
+++ Database.java
@@ -5,8 +5,20 @@ public static void main(String[] args)throws Exception
 {
 Class.forName("org.sqlite.JDBC");
 Connection con  = DriverManager.getConnection("jdbc:sqlite:univ.db");
-Statement stm = con.createStatement();
-stm.executeUpdate("delete from dept where deptno >= 60 ");
+PreparedStatement stm = con.prepareStatement("Insert into Student values (?,?,?,?)");
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter Student data: ");
+int r = sc.nextInt();
+String name  = sc.next();
+String city = sc.next();
+int deptno = sc.nextInt();
+
+stm.setInt(1,r);
+stm.setString(2,name);
+stm.setString(3,city);
+stm.setInt(4,deptno);
+stm.executeUpdate();
+
 stm.close();
 con.close();
 }
