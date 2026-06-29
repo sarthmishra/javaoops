aeadca186ad621ffc180cbb91e09768718a720bd
Sarth Mishra <mishrasarth2186@gmail.com>
2026-06-29 09:27:20 +0530

    Refactor Database class to remove student data insertion and create Temp table

--- Database.java
+++ Database.java
@@ -5,19 +5,8 @@ public static void main(String[] args)throws Exception
 {
 Class.forName("org.sqlite.JDBC");
 Connection con  = DriverManager.getConnection("jdbc:sqlite:univ.db");
-PreparedStatement stm = con.prepareStatement("Insert into Student values (?,?,?,?)");
-Scanner sc = new Scanner(System.in);
-System.out.print("Enter Student data: ");
-int r = sc.nextInt();
-String name  = sc.next();
-String city = sc.next();
-int deptno = sc.nextInt();
-
-stm.setInt(1,r);
-stm.setString(2,name);
-stm.setString(3,city);
-stm.setInt(4,deptno);
-stm.executeUpdate();
+Statement stm = con.createStatement();
+stm.executeUpdate("create table Temp(a int,b float)");
 
 stm.close();
 con.close();
