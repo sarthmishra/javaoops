ca3e4579e90460d1755ee984952264a28790b2f6
Sarth Mishra <mishrasarth2186@gmail.com>
2026-06-29 09:32:42 +0530

    Drop Temp table in Database class

--- Database.java
+++ Database.java
@@ -6,7 +6,7 @@ public static void main(String[] args)throws Exception
 Class.forName("org.sqlite.JDBC");
 Connection con  = DriverManager.getConnection("jdbc:sqlite:univ.db");
 Statement stm = con.createStatement();
-stm.executeUpdate("create table Temp(a int,b float)");
+stm.executeUpdate("drop table Temp");
 
 stm.close();
 con.close();
