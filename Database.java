be09e8c4238f813943f2c630f4082b737f5f17f0
Sarth Mishra <mishrasarth2186@gmail.com>
2026-06-30 11:48:52 +0530

    Remove Temp table drop and add student data retrieval in Database class

--- Database.java
+++ Database.java
@@ -6,7 +6,14 @@ public static void main(String[] args)throws Exception
 Class.forName("org.sqlite.JDBC");
 Connection con  = DriverManager.getConnection("jdbc:sqlite:univ.db");
 Statement stm = con.createStatement();
-stm.executeUpdate("drop table Temp");
+ResultSet rs = stm.executeQuery("Select * from student");
+while(rs.next()){
+int roll = rs.getInt("roll");
+String name = rs.getString("name");
+String city = rs.getString("city");
+int deptno = rs.getInt("deptno");
+System.out.println(roll + " " + name + " " + city + " " + deptno);
+}
 
 stm.close();
 con.close();
